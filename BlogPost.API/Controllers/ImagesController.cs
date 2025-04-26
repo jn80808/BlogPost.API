065b9dc64c67b8dd75c8dcdc809447f552a86a43
JN <joyng808@gmail.com>
2025-04-26 21:21:57 +0800

    [JN]- Added  Image File Validation

---
+++ BlogPost.API/Controllers/ImagesController.cs
@@ -0,0 +1,54 @@
+﻿using BlogPost.API.Model.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogPost.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+
+        //POST : {apibaseUrl}/api/images 
+
+        public async Task<IActionResult> UploadImage([FromForm] IFormFile file,
+            [FromForm] string fileName, [FromForm] string title)
+        {
+            ValidateFileUpload(file);
+
+            if(ModelState.IsValid) 
+            {
+                //File Upload
+                var blogImage = new BlogImage
+                { 
+                    FileExtension = Path.GetExtension(file.FileName).ToLower(),
+                    FileName = fileName,
+                    Title = title,
+                    DateCreated= DateTime.Now
+
+                };
+            }
+
+
+
+        }
+
+        private void ValidateFileUpload(IFormFile file)
+        {
+            var allowedExtensions = new string[] { ".jpeg",".jpg",".png" };
+
+            if (!allowedExtensions.Contains(Path.GetExtension(file.FileName).ToLower())) 
+            {
+                ModelState.AddModelError("file", "Unsupported file format");
+            }
+
+            if (file.Length > 10485760)
+            {
+                ModelState.AddModelError("file", "File size cannot be more than 10MB");
+            }
+
+        }
+
+
+    }
+}
